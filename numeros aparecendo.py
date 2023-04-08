927bca2d591451fea88dd82dfca5b600b745bd96
RecantoDoJohn <127706659+RecantoDoJohn@users.noreply.github.com>
2023-04-08 12:22:07 -0300

    Update numeros aparecendo.py

--- numeros aparecendo.py
+++ numeros aparecendo.py
@@ -1,12 +1,14 @@
+#the 'sleep' is just to make it mor pretty 
 from random import randint
 from time import sleep
 
+#variables: kind obvius
 numsrt = str(randint(100, 999))
 numesc = 0
 nmfinal = ('')
 x = 0
-print(numsrt)
 
+#this is were magic happens, this can also work with bigger numbers.
 while numsrt != nmfinal:
     for i in range (10):
         if str(i) != numsrt[x]:
